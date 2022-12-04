8a648665fd967cb39e9ef320459845d4df9c46eb
Admin <Admin@DESKTOP-JD7A7FJ>
2022-12-04 15:58:50 +0300

    added class:    TestSliderPage    TestProgressBarPage added tests:   test_slider   test_progress_bar

--- pages/base_page.py
+++ pages/base_page.py
@@ -53,4 +53,8 @@ class BasePage:
         hundle=self.driver.window_handles[number_hundle]
         self.driver.switch_to.window(hundle)
 
+    def action_drag_and_drop_by_offset(self, element, x_coords, y_coords):
+        action = ActionChains(self.driver)
+        action.drag_and_drop_by_offset(element, x_coords, y_coords)
+        action.perform()
 
